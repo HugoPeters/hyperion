c14dd80fe56713932ea1fe51128b1c2c822cc8a7
Hugo Peters <mail@hugo.fyi>
2018-04-25 01:20:39 +0200

    Test

--- src/hyperiond/hyperiond.cpp
+++ src/hyperiond/hyperiond.cpp
@@ -501,6 +501,7 @@ int main(int argc, char** argv)
 
 
 	startBootsequence(config, hyperion);
+	hyperion.setEffect("udp", 1, -1);
 
 	XBMCVideoChecker * xbmcVideoChecker = nullptr;
 	startXBMCVideoChecker(config, xbmcVideoChecker);
